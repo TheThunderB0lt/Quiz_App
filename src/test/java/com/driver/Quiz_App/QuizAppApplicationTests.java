cbce2ee51158c960080ae7212b35fbe1a1efe802
TheThunderB0lt <nikithkumarniki160698@gmail.com>
2024-03-20 12:40:08 +0530

    first commit

---
+++ src/test/java/com/driver/Quiz_App/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driver.Quiz_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
